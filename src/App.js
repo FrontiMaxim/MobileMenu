271a83ebebc47cc733d4dc0be634c347f652dd0e
FrontiMaxim <FrontiMax@yandex.ru>
2021-08-24 11:31:59 +0400

    adding change of activity more

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import {createRef} from 'react';
 import MenuItem from './Components/MenuItem';
 import './App.css';
 
 function App() {
     let socialLinks = ['fab fa-instagram', 'fab fa-youtube', 'fab fa-whatsapp'];
+    let refMore = createRef();
+
+    let changeActive = () => {
+        refMore.current.classList.toggle('active');
+    }
 
     return (
         <div className="nav-bar">
-            <div className="more">
+            <div className="more" ref={refMore} onClick={changeActive}>
                 <div className="social-links">
                     {
                         socialLinks.map((classValue, key) => <MenuItem key={key} classValue={classValue} />)
